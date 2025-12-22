127075b393c788fd7e37dfa127dcde814a70ccf0
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-12-22 22:10:59 +0600

    Watermelon

---
+++ Watermelon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>2&&
+       n%2==0&&n!=0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
