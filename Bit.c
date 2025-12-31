ad2cd953a5ab91d1fd287a5b24600e170376a5f8
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-12-31 21:26:10 +0600

    Update

--- Bit.c
+++ Bit.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,i,b=0;
+    char a[4];
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%s",&a);
+        if(a[1]=='+')
+        {
+            b++;
+        }
+        else
+        {
+            b--;
+        }
+    }
+    printf("%d",b);
+    return 0;
+}
