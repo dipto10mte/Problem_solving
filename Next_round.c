0a1186464446a64a24c9fcdca1bd0f3f866fdcf5
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2025-12-31 21:00:37 +0600

    Update

--- Next_round.c
+++ Next_round.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,i,a,b=0;
+    int n,k,l,i,b=0;
     scanf("%d %d",&n,&k);
+    int a[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a);
-        if(a>=k)
+        scanf("%d",&a[i]);
+    }
+    l=a[k-1];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=l&&a[i]>0)
         {
             b++;
         }
