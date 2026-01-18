0a9cfa4ab30dd78d015beaa23e9f3acde66976ef
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-18 23:15:38 +0600

    Update

--- Bear_and_big_brother.c
+++ Bear_and_big_brother.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,i=0;
+    scanf("%d %d",&a,&b);
+    while(1)
+    {
+        a=3*a;
+        b=2*b;
+        i++;
+        if(a>b)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    printf("%d",i);
     return 0;
 }
