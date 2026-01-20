245f8e22e18fb91139b2f9d3d1da129b4fb84221
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-20 23:39:45 +0600

    Update

--- Stones_on_the_table.c
+++ Stones_on_the_table.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
-iny main()
+int main()
 {
-
+    int n,i,a=0;
+    char s[51];
+    scanf("%d",&n);
+    scanf("%s",s);
+    for(i=0;i<n;i++)
+    {
+        if(s[i]==s[i+1])
+        {
+            a++;
+        }
+    }
+    printf("%d",a);
     return 0;
 }
 
