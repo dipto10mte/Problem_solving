bb727f3b90540efb6d0b902ed4e56d13bc016ae2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-22 16:39:27 +0600

    Anton_and_danik

--- Anton_and_danik.c
+++ Anton_and_danik.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
+    int i,n,x=0,y=0;
+    char a[100001];
+    scanf("%d",&n);
+    scanf("%s",&a);
+    for(i=0;i<strlen(a);i++)
+    {
+        if(a[i]=='A')
+        {
+            x++;
+        }
+        if(a[i]=='D')
+        {
+            y++;
+        }
 
+    }
+    if(x>y)
+    {
+        printf("Anton");
+    }
+    else if(y>x)
+    {
+        printf("Danik");
+    }
+    else
+    {
+        printf("Friendship");
+    }
     return 0;
 }
