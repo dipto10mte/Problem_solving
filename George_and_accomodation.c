f05dac9ccd4c94f899b4a23f2ee3023ee5815fdc
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-25 23:23:11 +0600

    Update

--- George_and_accomodation.c
+++ George_and_accomodation.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main()
 {
-
+    int a=0,i,n,p,q;
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d %d",&p,&q);
+        if(q-p>1)
+        {
+            a++;
+        }
+    }
+    printf("%d",a);
     return 0;
 }
