cb0e1a920724d241b75a576847e7ec64cc1655c9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-25 23:23:20 +0600

    Update

--- Calculating_function.c
+++ Calculating_function.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 int main()
 {
-
+    int b=0,i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            b=b+i;
+        }
+        else
+        {
+            b=b-i;
+        }
+    }
+    printf("%d",b);
     return 0;
 }
