176cdc3f87d176d8663f5a060e51e7e55f049417
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-29 21:33:33 +0600

    Update

--- Is_your_horseshoe.c
+++ Is_your_horseshoe.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int i,a[4];
-    for(i=0;i<4;i++)
+    int i,p,q,r,s;
+    scanf("%d %d %d %d",&p,&q,&r,&s);
+    if(p==q&&q==r&&r==s)
     {
-        scanf("%d",&a[i]);
+        printf("3");
+    }
+    else if((p==q&&q==r)||(p==q&&q==s)||(p==q&&r==s)||(p==r&&q==s)||(p==s&&q==r)||(q==r&&r==s))
+    {
+        printf("2");
+    }
+    else if(p==q||p==r||p==s||q==r||q==s||r==s)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
     }
     return 0;
 }
