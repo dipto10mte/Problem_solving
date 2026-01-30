862629b87e786864c326f39d55134dd425465a63
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-01-30 23:24:42 +0600

    Update

--- Pangram.c
+++ Pangram.c
@@ -1,6 +1,44 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,j,n,x=0,y=0;
+    char s[101];
+    scanf("%d",&n);
+    if(n<26)
+    {
+        printf("NO");
+        return 0;
+    }
+    scanf("%s",&s);
+    for(i=0;i<n;i++)
+    {
+        if(s[i]<97)
+        {
+            s[i]=s[i]+32;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        y=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(s[i]==s[j])
+            {
+                y++;
+            }
+        }
+        if(y==0)
+        {
+            x++;
+        }
+    }
+    if(x==26)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
     return 0;
 }
