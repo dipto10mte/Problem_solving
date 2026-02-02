4e78081948fb2f9f59d05f42d6a073d2975a1708
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-02 23:53:02 +0600

    Update

--- Arrival_of_the_general.c
+++ Arrival_of_the_general.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,n,x=0,y=0,a[101];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int max=a[0],min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            x=i;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=min)
+        {
+            min=a[i];
+            y=i;
+        }
+    }
+    if(x>y)
+    {
+        printf("%d",x+n-y-2);
+    }
+    else
+    {
+        printf("%d",x+n-y-1);
+    }
     return 0;
 }
