40c1059b072669f99b884ff3dcd9a8b189692911
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-02 23:53:09 +0600

    Update

--- Hit_the_lottery.c
+++ Hit_the_lottery.c
@@ -1,6 +1,32 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,n=0;
+    scanf("%d",&a);
+    if(a>99)
+    {
+        n=n+a/100;
+        a=a%100;
+    }
+    if(a>19)
+    {
+        n=n+a/20;
+        a=a%20;
+    }
+    if(a>9)
+    {
+        n=n+a/10;
+        a=a%10;
+    }
+    if(a>4)
+    {
+        n=n+a/5;
+        a=a%5;
+    }
+    if(a<5)
+    {
+        n=n+a;
+    }
+    printf("%d",n);
     return 0;
 }
