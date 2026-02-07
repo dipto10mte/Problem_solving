90427651c4f78dbd22723adc2e868c839ed3877d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-07 23:09:58 +0600

    Update

--- Games.c
+++ Games.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,j,n,m=0,a[101],b[101];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==b[j])
+            {
+                m++;
+            }
+        }
+    }
+    printf("%d",m);
     return 0;
 }
