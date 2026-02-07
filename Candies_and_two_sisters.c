72a5af22a49bf8fd1ca9706be3227161c3a60298
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-07 23:09:49 +0600

    Update

--- Candies_and_two_sisters.c
+++ Candies_and_two_sisters.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,n,a[10001],b[10001];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]==0||a[i]==1||a[i]==2)
+        {
+            b[i]=0;
+        }
+        else if(a[i]%2==0)
+        {
+            b[i]=a[i]/2-1;
+        }
+        else
+        {
+            b[i]=(a[i]-1)/2;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",b[i]);
+    }
     return 0;
 }
