9a9ce4cb44e3cec416e73436a513beaac6a1cd4b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-15 23:54:41 +0600

    How_much_does_daytona_cost

--- How_much_does_daytona_cost.c
+++ How_much_does_daytona_cost.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 int main()
 {
-
+    int a[100],b[1000]={0},i,j,k,n,t;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        scanf("%d %d",&n,&k);
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i]);
+            if(a[i]==k)
+                b[i]=1;
+        }
+    }
+    for(i=0;i<t;i++)
+    {
+        if(b[i]==1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
     return 0;
 }
