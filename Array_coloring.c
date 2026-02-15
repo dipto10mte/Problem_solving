5d29042af92e021b02d1721c138121aed2df52d9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-15 23:54:14 +0600

    Array_coloring

--- Array_coloring.c
+++ Array_coloring.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,i,j,m,n,t;
+    scanf("%d",&t);
+    int b[t];
+    for(i=0;i<t;i++)
+    {
+        m=0;
+        scanf("%d",&n);
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a);
+            if(a%2!=0)
+                m++;
+        }
+        b[i]=m;
+    }
+    for(i=0;i<t;i++)
+    {
+        if(b[i]%2==0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
     return 0;
 }
