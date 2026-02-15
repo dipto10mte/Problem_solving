1acb8d5e7715706ca6c2b5b48ddf61226cef5e6b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-15 23:54:05 +0600

    Odd_one_out

--- odd_one_out.c
+++ odd_one_out.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,c,i,t;
+    scanf("%d",&t);
+    int x[t];
+    for(i=0;i<t;i++)
+    {
+        scanf("%d %d %d",&a,&b,&c);
+        if(a==b)
+            x[i]=c;
+        else if(a==c)
+            x[i]=b;
+        else
+            x[i]=a;
+    }
+    for(i=0;i<t;i++)
+    {
+        printf("%d\n",x[i]);
+    }
     return 0;
 }
