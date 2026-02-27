2be78815efb0d2ef73bf5000a20ae5d7b1e4164c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-27 23:10:44 +0600

    Update

--- Holiday_of_equality.c
+++ Holiday_of_equality.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main()
 {
-
+    int a[100],b=0,i,m=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]>m)
+            m=a[i];
+    }
+    for(i=0;i<n;i++)
+        b=b+m-a[i];
+    printf("%d",b);
     return 0;
 }
