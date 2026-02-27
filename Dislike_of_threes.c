109977a1c78ac1c50e0af1e448cb7bbfd0bc0897
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-27 23:10:27 +0600

    Update

--- Dislike_of_threes.c
+++ Dislike_of_threes.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
-
+    int a[100],i,k,m,n,t;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        scanf("%d",&k);
+        for(m=0,n=0;m<k;m++,n++){
+            if(n%3==0||n%10==3)
+                m--;
+        }
+        a[i]=--n;
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
