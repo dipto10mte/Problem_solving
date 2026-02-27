e819c8295d2e5f39a442c9100c87b5ba19cae30d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-27 23:10:19 +0600

    Update

--- Blank_space.c
+++ Blank_space.c
@@ -1,5 +1,37 @@
+
 #include<stdio.h>
 int main()
 {
+    int a,b[1000]={0},i,j,m,n,s,t;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        s=0;
+        scanf("%d",&n);
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a);
+            if(a==0)
+                s++;
+            else
+            {
+                if(b[i]<=s)
+                {
+                    b[i]=s;
+                    s=0;
+                }
+            }
+            if(j==n-1)
+            {
+                if(b[i]<=s)
+                {
+                    b[i]=s;
+                    s=0;
+                }
+            }
+        }
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",b[i]);
     return 0;
 }
