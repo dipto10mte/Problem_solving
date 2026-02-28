600134d8050514ff444524f042d8c34491913199
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-28 21:59:38 +0600

    Update

--- To_my_critics.c
+++ To_my_critics.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,c,i,t,x[1000]={0};
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        scanf("%d %d %d",&a,&b,&c);
+        if(a+b>=10||b+c>=10||c+a>=10)
+            x[i]=1;
+    }
+    for(i=0;i<t;i++)
+    {
+        if(x[i]==1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
     return 0;
 }
