fcc7b93bbafec94b931d24d61af32fa4f1749cf4
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-02-28 22:00:20 +0600

    Divisibility_problem

---
+++ Divisibility problem.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[1000],x,y,z,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&x,&y);
+        if(x%y==0)
+        {
+            a[i]=0;
+        }
+        else
+        {
+        z=x/y;
+        a[i]=y-(x-z*y);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
+
