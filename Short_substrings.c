4dcb7d0c0f7b4e95f8b2f48b5baf4f752e4a9992
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-03 12:05:37 +0600

    Short_substrings

---
+++ Short_substrings.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int c[1000],i,t;
+    char a[100],b[100];
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%s",b);
+        if(strlen(b)<3)
+
+    }
+    return 0;
+}
