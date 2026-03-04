a0bf0a850b5a94343f9b7f0227018e48e9e33149
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-04 17:46:03 +0600

    Update

--- Mishka_and_game.c
+++ Mishka_and_game.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,i,n,p=0,q=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        if(a>b)
+            p++;
+        else if(b>a)
+            q++;
+    }
+    if(p>q)
+        printf("Mishka");
+    else if(q>p)
+        printf("Chris");
+    else
+        printf("Friendship is magic!^^");
     return 0;
 }
