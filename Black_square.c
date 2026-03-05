de90aff5e4c46b11725a414f885dfacd9a4e71e2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-05 15:36:10 +0600

    Black_square

--- Black_square.c
+++ Black_square.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-
+    int a,b,c,d,i,n=0;
+    char x[100001];
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    scanf("%s",x);
+    for(i=0;i<strlen(x);i++){
+        if(x[i]=='1')
+            n+=a;
+        else if(x[i]=='2')
+            n+=b;
+        else if(x[i]=='3')
+            n+=c;
+        else
+            n+=d;
+    }
+    printf("%d",n);
     return 0;
 }
