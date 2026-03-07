a4b7c1e44501f5fb4087c996182cc12df1495163
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-07 23:02:42 +0600

    Required_reminder

--- Required_reminder.c
+++ Required_reminder.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,n,t,x,y;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d %d %d",&x,&y,&n);
+        while(1){
+            if(n%x==y){
+                a[i]=n;
+                break;
+            }
+            n--;
+        }
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
