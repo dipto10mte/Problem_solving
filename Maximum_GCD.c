a9521fd543a95eb39d6d0898a710b9b9eee52119
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-07 23:02:24 +0600

    Maximum_GCD

--- Maximum_GCD.c
+++ Maximum_GCD.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,n,t;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&a[i]);
+        a[i]=a[i]/2;
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
