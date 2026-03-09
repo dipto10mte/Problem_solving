a4f72a19073e37ecbae43a1cac92f7a1fa096ae9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-09 17:28:24 +0600

    Update

--- Yet_another_two_integer_problems.c
+++ Yet_another_two_integer_problems.c
@@ -4,7 +4,16 @@ int main()
     int a,b,i,n,t;
     scanf("%d",&t);
     int x[t];
+    for(i=0;i<t;i++)
+    {
+        n=0;
+        scanf("%d %d",&a,&b);
+        if(a==b)
+        {
+            x[i]=0;
+        }
 
+    }
     for(i=0;i<t;i++)
         printf("%d\n",x[i]);
     return 0;
