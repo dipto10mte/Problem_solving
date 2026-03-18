8eb7531e017cd8a319edf7cbb75015d1fc3992ae
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-18 14:07:04 +0600

    Easy_problem

---
+++ Easy_problem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,t;
+    scanf("%d",&t);
+    int a[i];
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        a[i]=n-1;
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
