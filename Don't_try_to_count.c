1f816d684cbf51401f4d2fc72ceba2deb88a4ce9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-20 12:58:56 +0600

    Don't_try_to_count

---
+++ Don't_try_to_count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,m,n,t;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i+=2){
+        scanf("%d %d",&n,&m);
+        char x[n],s[m];
+    }
+    for(i=0;i<t;i+=2)
+        printf("%d\n",a[i]);
+    return 0;
+}
