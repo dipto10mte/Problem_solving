685b78bff510f787f4d878fa7b24c1962a53b56c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-21 21:22:10 +0600

    Minimal_square

--- Minimal_square.c
+++ Minimal_square.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,b,i,t,x,y;
+    scanf("%d",&t);
+    int z[t];
+    for(i=0;i<t;i++){
+        scanf("%d %d",&a,&b);
+        if(a<b){
+            x=a;
+            y=b;
+        }
+        else{
+            x=b;
+            y=a;
+        }
+        if(2*x>=y)
+            z[i]=4*x*x;
+        else
+            z[i]=y*y;
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",z[i]);
     return 0;
 }
 
