f65040fd9d3ada2e452c90c2c69fb0951589d795
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-25 11:49:15 +0600

    Update

--- Skibidus_and_amog'u.c
+++ Skibidus_and_amog'u.c
@@ -2,19 +2,28 @@
 #include<string.h>
 int main()
 {
-    /*int i,t;
+    int i,j,k=0,m=0,n,t;
     scanf("%d",&t);
     int a[t];
-    char w[10*t];
+    char w[10],b[10*t];
     for(i=0;i<t;i++){
         scanf("%s",w);
         n=strlen(w);
         w[n-2]='i';
         w[n-1]='\0';
+        n=strlen(w);
+        m+=n;
+        a[i]=n;
+        for(j=0;j<n;j++,k++)
+            b[k]=w[j];
     }
-    printf("%s",w);*/
-    char a,b;
-    scanf("%c %c",&a,&b);
-    printf("%c",a+b);
+    for(i=0,j=0,k=0;i<m;i++,k++){
+        if(k==a[j]){
+            printf("\n");
+            j++;
+            k=0;
+        }
+        printf("%c",b[i]);
+    }
     return 0;
 }
