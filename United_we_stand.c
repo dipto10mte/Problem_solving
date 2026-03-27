74bb823e53f863b18ea6e3fb809da44722b5f28d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-27 15:09:42 +0600

    Update

--- United_we_stand.c
+++ United_we_stand.c
@@ -1,9 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,t,x,y;
+    int b[50000],e,i,j,k,m,n,o=0,t,x,y;
     scanf("%d",&t);
     int a[2*t];
-    for(i=0;i<t;i++)
+    for(i=0;i<t;i+=2){
+        scanf("%d",&n);
+        int d[n];
+        m=1;
+        x=0;
+        y=0;
+        for(j=0;j<n;j++)
+            scanf("%d",&d[j]);
+        for(j=0;j<n;j++){
+            for(k=j+1;k<n;k++){
+                if(d[j]>d[k]){
+                    e=d[j];
+                    d[j]=d[k];
+                    d[k]=e;
+                }
+            }
+        }
+        for(j=1;j<n;j++){
+            if(d[j-1]==d[j])
+                m++;
+        }
+        if(m==n){
+            a[i]=-1;
+            i--;
+        }
+        else{
+            o+=n;
+
+        }
+    }
     return 0;
 }
