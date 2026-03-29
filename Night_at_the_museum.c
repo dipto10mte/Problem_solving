82d6f4d72282b2361bd6af8a7c9806212baa04e1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-29 19:59:33 +0600

    Night_at_the_museum

---
+++ Night_at_the_museum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,m,n=0,0=1;
+    char a[101];
+    scanf("%s",a);
+    for(i=0;i<strlen(a);i++){
+        m=a[i]-96;
+    }
+    printf("%d",n);
+    return 0;
+}
+
