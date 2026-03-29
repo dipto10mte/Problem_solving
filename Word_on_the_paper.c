7f2d77572ee2517a8ccf3f7b7d6f6e818f3c1475
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-29 19:59:16 +0600

    Update

--- Word_on_the_paper.c
+++ Word_on_the_paper.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,m=0,n,t;
+    char b[64000],c[8];
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++){
+        for(j=0;j<8;j++){
+            scanf(" %c",c);
+        }
+    }
     return 0;
 }
