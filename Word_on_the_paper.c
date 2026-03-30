8880051d72ec0b5c8706fcbd49194ea0b5063b15
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-03-30 22:51:45 +0600

    Update

--- Word_on_the_paper.c
+++ Word_on_the_paper.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int i,m=0,n,t;
-    char b[64000],c[8];
+    int i,j,m,n=0,t;
+    char b[64000],c;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
-        for(j=0;j<8;j++){
-            scanf(" %c",c);
+        m=n;
+        for(j=0;j<64;j++){
+            scanf(" %c",&c);
+            if(c!='.'){
+                b[n]=c;
+                n++;
+            }
         }
+        a[i]=n-m;
+    }
+    n=0;
+    for(i=0;i<t;i++){
+        for(j=0;j<a[i];j++,n++)
+            printf("%c",b[n]);
+        printf("\n");
     }
     return 0;
 }
