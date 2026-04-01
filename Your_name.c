fe433e8fe7a2869791f0b1cf827fc41ab6dea6a4
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-01 19:39:48 +0600

    Your_name

---
+++ Your_name.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,t;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        char b[n],c[n];
+        a[i]=1;
+        int d[26]={0},e[26]={0};
+        scanf("%s %s",b,c);
+        for(j=0;j<n;j++)
+            d[b[j]-97]++;
+        for(j=0;j<n;j++)
+            e[c[j]-97]++;
+        for(j=0;j<26;j++){
+            if(d[j]!=e[j]){
+                a[i]=0;
+                break;
+            }
+        }
+    }
+    for(i=0;i<t;i++){
+        if(a[i]==1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
