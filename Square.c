cfaa315954d4ece7005bd8c00247f977f232db7e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-03 12:24:39 +0600

    Update

--- Square.c
+++ Square.c
@@ -1,21 +1,53 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d,i,t;
+    int i,t,x,x1,x2,x3,x4,y,y1,y2,y3,y4;
     scanf("%d",&t);
-    int x[t];
+    int a[t];
     for(i=0;i<t;i++){
-        scanf("%d %d %d %d",&a,&b,&c,&d);
-        if(a==b&&b==c&&c==d)
-            x[i]=1;
-        else
-            x[i]=0;
+        scanf("%d %d",&x1,&y1);
+        scanf("%d %d",&x2,&y2);
+        scanf("%d %d",&x3,&y3);
+        scanf("%d %d",&x4,&y4);
+        if(x1==x2){
+            if(y1>y2)
+                y=y1-y2;
+            else
+                y=y2-y1;
+        }
+        else if(x1==x3){
+            if(y1>y3)
+                y=y1-y3;
+            else
+                y=y3-y1;
+        }
+        else{
+            if(y1>y4)
+                y=y1-y4;
+            else
+                y=y4-y1;
+        }
+        if(y1==y2){
+            if(x1>x2)
+                x=x1-x2;
+            else
+                x=x2-x1;
+        }
+        else if(y1==y3){
+            if(x1>x3)
+                x=x1-x3;
+            else
+                x=x3-x1;
+        }
+        else{
+            if(x1>x4)
+                x=x1-x4;
+            else
+                x=x4-x1;
+        }
+        a[i]=x*y;
     }
-    for(i=0;i<t;i++){
-        if(x[i]==1)
-            printf("YES\n");
-        else
-            printf("NO\n");
-    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
