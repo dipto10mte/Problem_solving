d2747bae87f9213cc986504c6a02d4abab1d91e7
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-06 18:52:12 +0600

    Update

--- Array_with_odd_sum.c
+++ Array_with_odd_sum.c
@@ -4,6 +4,19 @@ int main()
     int i,j,m,n,o,t;
     scanf("%d",&t);
     int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        m=0;
+        for(j=0;j<n;j++){
+            scanf("%d",&o);
+            if(o%2==1)
+                m++;
+        }
+        if(m%2==1)
+            a[i]=1;
+        else
+            a[i]=0;
+    }
 
     return 0;
 }
