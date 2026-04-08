77bcf29823a4b75e2f992b48e18d94ca200038d2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-08 23:12:26 +0600

    Update

--- Ordinary_numbers.c
+++ Ordinary_numbers.c
@@ -5,6 +5,20 @@ int main()
     long long int n,x,y;
     scanf("%d",&t);
     int a[t];
+    for(i=0;i<t;i++){
+        scanf("%lld",&n);
+        x=0;
+        m=0;
+        for(j=0;j<=9;j++){
+            x=x*10+1;
+            for(k=1;k<=9;k++){
+                y=x*k;
+                if(y<=n)
+                    m++;
+            }
+        }
+        a[i]=m;
+    }
 
     return 0;
 }
