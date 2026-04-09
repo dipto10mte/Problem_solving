2da0fb5481cdd46112ed5b0e277e5fadd5a5a88a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-09 19:01:09 +0600

    Update

--- Perfect_permutation.c
+++ Perfect_permutation.c
@@ -5,6 +5,10 @@ int main()
     scanf("%d",&n);
     if(n%2==1)
         printf("-1");
-
+    else{
+        for(i=1;i<=n;i+=2){
+            printf("%d %d ",i+1,i);
+        }
+    }
     return 0;
 }
