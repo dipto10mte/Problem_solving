30ac66ea8bc5834e51d7e9a492b3a4de73356b99
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-10 22:04:54 +0600

    Update

--- Make_it_beautiful.c
+++ Make_it_beautiful.c
@@ -1,4 +1,24 @@
 #include<stdio.h>
+
+int check(int arr[],int n){
+    int i,m=0;
+    for(i=0;i<=n;i++){
+        if(m==arr[i])
+            return 0;
+        m+=arr[i];
+    }
+    return 1;
+}
+
+int swap(int arr[],int n){
+    int i,m;
+    for(i=1;i<=n;i++){
+        m=arr[i-1];
+        arr[i-1]=arr[i];
+        arr[i]=m;
+    }
+}
+
 int main()
 {
 
