0ff6506c8fc5a3fbb0c98cdb3bc2fc25f2151697
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-10 18:28:07 +0600

    Update

--- Sum_of_round_numbers.c
+++ Sum_of_round_numbers.c
@@ -1,33 +1,45 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,i,j,n,p,q;
+    int a,b,c,i,j,k,n,p,q;
     scanf("%d",&n);
     int x[n];
     for(i=0;i<n;i++)
-    {
         scanf("%d",&x[i]);
-    }
     for(i=0;i<n;i++)
     {
-        a=x[i];
+        c=x[i];
         p=0;
         q=0;
-        while(a>0)
+        b=0;
+        while(c>0)
         {
-            c=a%10;
+            a=c%10;
             q++;
-            if(c!=0)
+            if(a!=0)
             {
                 p++;
             }
-            a=a/10;
-            for(j=0;j<q&&c!=0;j++)
-            {
-
-            }
+            c=c/10;
+            b=b*10+a;
         }
-        printf("%d");
+        printf("%d\n",p);
+        int d[p];
+        for(j=0;j<p;j++){
+            a=b%10;
+            b=b/10;
+            if(a!=0){
+                for(k=1;k<q;k++)
+                    a=a*10;
+                d[j]=a;
+            }
+            else
+                j--;
+            q--;
+        }
+        for(j=0;j<p;j++)
+            printf("%d ",d[j]);
+        printf("\n");
     }
     return 0;
 }
