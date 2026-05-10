18cecd9b82d649c133e8a70a70f4c2217de0ed7b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-10 18:28:27 +0600

    Update

--- Sereja_and_dima.c
+++ Sereja_and_dima.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,x=0,y=0;
+    int i,j,k,n,x=0,y=0;
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++)
-    {
         scanf("%d",&a[i]);
-    }
-    j=n-1;
-    for(i=0;i<(n+1)/2;i++,j--)
-    {
-        if(i==j)
+    for(i=0,j=0,k=n-1;i<n;i++){
+        if(i%2==0)
         {
-            x+=a[i];
-            break;
-        }
-        if(a[i]>a[j])
-        {
-            x+=a[i];
-            if(a[i+1])
+            if(a[j]>a[k]){
+                x+=a[j];
+                j++;
+            }
+            else{
+                x+=a[k];
+                k--;
+            }
         }
         else
         {
-            x+=a[j];
-            y+=a[i];
+            if(a[j]>a[k]){
+                y+=a[j];
+                j++;
+            }
+            else{
+                y+=a[k];
+                k--;
+            }
         }
 
     }
