77c3f8eab3f2428dc81d9c344a05da1efd745043
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-14 12:58:51 +0600

    Update

--- Spell_check.c
+++ Spell_check.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    int i,n,t;
-    char s[6]={'a','b','c','d','e','\0'};
+    int i,j,k,n,t;
+    char d[]={'T','i','m','u','r','\0'};
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%d",&n);
-        char b[n];
+        char b[n+1];
         scanf("%s",b);
-        if(n!=5){
+        int c[5]={0};
+        if(n!=5)
             a[i]=0;
-            continue;
+        else{
+            for(j=0;j<5;j++){
+                for(k=0;k<5;k++){
+                    if(b[j]==d[k])
+                        c[k]++;
+                }
+            }
         }
-        else if(b='T','i','m','u','r')
+        if(c[0]==1&&c[1]==1&&c[2]==1&&c[3]==1&&c[4]==1)
             a[i]=1;
-        else if(b='T','i','m','r','u')
-            a[i]=1;
-        else if(b='T','i','u','m','r')
-            a[i]=1;
-        else if(b='T','i','u','r','m')
-            a[i]=1;
+        else
+            a[i]=0;
     }
     for(i=0;i<t;i++){
         if(a[i]==1)
-            printf("YES");
+            printf("YES\n");
         else
-            printf("NO");
+            printf("NO\n");
     }
     return 0;
 }
