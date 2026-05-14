6d92c6577ac7c2e1d5147e87d6cf0f49ddab3fca
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-14 12:58:58 +0600

    Update

--- Two_arrays_and_swaps.c
+++ Two_arrays_and_swaps.c
@@ -1,43 +1,38 @@
 #include<stdio.h>
+
+void sorting(int arr[],int n){
+    int i,j,x;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]<arr[j]){
+                x=arr[i];
+                arr[i]=arr[j];
+                arr[j]=x;
+            }
+        }
+    }
+}
 int main()
 {
-    int i,j,k,l,m,n,t,x;
+    int i,j,k,l,n,t;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%d %d",&n,&k);
         int b[n],c[n];
         a[i]=0;
-        m=0;
+        l=n-1;
         for(j=0;j<n;j++)
             scanf("%d",&b[j]);
         for(j=0;j<n;j++)
             scanf("%d",&c[j]);
-        for(j=0;j<n;j++){
-            for(l=j+1;l<n;l++){
-                if(b[j]<b[l]){
-                    x=b[j];
-                    b[j]=b[l];
-                    b[l]=x;
-                }
-            }
+        sorting(b,n);
+        sorting(c,n);
+        for(j=0;j<k;j++,l--){
+            if(c[j]>b[l])
+                b[l]=c[j];
         }
-        for(j=0;j<n;j++){
-            for(l=j+1;l<n;l++){
-                if(c[j]<c[l]){
-                    x=c[j];
-                    c[j]=c[l];
-                    c[l]=x;
-                }
-            }
-        }
-        for(j=0;j<k;j++){
-            if(c[j]>b[j]){
-                a[i]+=c[j];
-                m++;
-            }
-        }
-        for(j=0;j<n-m;j++)
+        for(j=0;j<n;j++)
             a[i]+=b[j];
     }
     for(i=0;i<t;i++)
