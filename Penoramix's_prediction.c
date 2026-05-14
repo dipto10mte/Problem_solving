7b916a46ac0f614d1776b11dac4db007fd2d5621
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-14 12:58:38 +0600

    Update

--- Penoramix's_prediction.c
+++ Penoramix's_prediction.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,j;
+    int a,b,i,j,m,n;
     scanf("%d %d",&a,&b);
     i=b+1;
-    if(b%2==0){
-        printf("No");
-        return 0;
-    }
-    else if(b==3){
-        printf("YES");
-        return 0;
+    if(b%2==0)
+        printf("NO");
+    else if(a==2){
+        if(b==3)
+            printf("YES");
+        else
+            printf("NO");
     }
     else{
-        for(i=a+2;i<b;i+=2){
-            for(i=2;i*i<j;i++){
-                if(b%i==0){
-                    break;
-                }
-                else{
-                    printf("No");
-                    return 0;
-                }
+        for(i=a+2;i<=b;i+=2){
+                n=0;
+            for(j=3;j<i;j+=2){
+                if(i%j==0)
+                    n++;
+            }
+            if(n==0){
+                m=i;
+                break;
             }
         }
+        if(m==b)
+            printf("YES");
+        else
+            printf("NO");
     }
-    printf("YES");
     return 0;
 }
