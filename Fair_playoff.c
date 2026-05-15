8ab89683281c739dab046d04e5b5c733a0de1afa
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-15 11:31:34 +0600

    Update

--- Fair_playoff.c
+++ Fair_playoff.c
@@ -1,36 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j,k,m,n,t,x[4];
+    int a,b,c,d,i,t;
     scanf("%d",&t);
     int s[t];
     for(i=0;i<t;i++){
-        for(j=0;j<4;j++){
-            scanf("%d",&x[i]);
-        }
-        if(x[0]>x[1])
-            m=x[0];
-        else
-            m=x[1];
-        if(x[2]>x[3])
-            n=x[2];
-        else
-            n=x[3];
-        for(j=0;j<4;j++){
-            for(k=j+1;k<4;k++){
-                if(x[j]>x[k]){
-                    a=x[j];
-                    x[j]=x[k];
-                    x[k]=a;
-                }
-            }
-        }
-        if(m==x[2]&&n==x[3])
-            s[i]=1;
-        else if(m==x[3]&&n==x[2])
-            s[i]=1;
-        else
+        scanf("%d %d %d %d",&a,&b,&c,&d);
+        if(c>a&&c>b&&d>a&&d>b)
             s[i]=0;
+        else if(a>c&&a>d&&b>c&&b>d)
+            s[i]=0;
+        else
+            s[i]=1;
     }
     for(i=0;i<t;i++){
         if(s[i]==1)
