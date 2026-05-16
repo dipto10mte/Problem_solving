047ec6b3ffa8171b98c4cc93cb6a93f68252fba5
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-16 10:34:05 +0600

    Update

--- Doremy's_paint_3.c
+++ Doremy's_paint_3.c
@@ -1,28 +1,42 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,m,n,t;
+    int i,j,l,m,n,t,x,y;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%d",&n);
         int b[n];
         m=0;
+        l=0;
         for(j=0;j<n;j++)
             scanf("%d",&b[j]);
+        x=b[0];
         for(j=1;j<n;j++){
-                if(b[j-1]!=b[j])
-                    m++;
+                if(b[j]!=x){
+                    y=b[j];
+                    break;
+                }
         }
-        if(n==2)
-            a[i]=1;
-        else if(m==0)
-            a[i]=1;
-        else if(n==3&&m==1)
-            a[i]=1;
-        else
+        for(j=0;j<n;j++){
+                if(b[j]!=x&&b[j]!=y){
+                        m++;
+                }
+        }
+        if(m>0)
             a[i]=0;
-
+        else{
+            for(j=0;j<n;j++){
+                if(b[j]==x)
+                    m++;
+                else
+                    l++;
+            }
+            if(m==l||m-l==1||l-m==1||l==0||m==0)
+                a[i]=1;
+            else
+                a[i]=0;
+        }
     }
     for(i=0;i<t;i++){
         if(a[i]==1)
