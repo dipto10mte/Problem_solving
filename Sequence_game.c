20baf5fc5dfa17e2b74b60f2af45419212ff1838
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-16 10:33:47 +0600

    Update

--- Sequence_game.c
+++ Sequence_game.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int b[200000],i,j,m=0,n,o,t;
+    int b[400005],i,j,m=0,n,o,t;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%d",&n);
         int c[n];
         a[i]=m;
-        for(j=0;j<n;j++){
+        for(j=0;j<n;j++)
             scanf("%d",&c[j]);
-        }
         for(j=0;j<n;j++,m++){
             b[m]=c[j];
-            if(c[j]>c[j+1]){
+            if(j<n-1&&c[j]>c[j+1]){
                 b[m+1]=c[j+1];
                 m++;
             }
