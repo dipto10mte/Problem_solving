23d89cac7df982f76c89855ecacd27bd6ee9649b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-17 10:58:17 +0600

    Update

--- Coins.c
+++ Coins.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,k,n,t;
+    int i,t;
+    long long k,n;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
-        scanf("%d %d",&n,&k);
-        if(n%k==0)
+        scanf("%lld %lld",&n,&k);
+        if(n%2==0)
             a[i]=1;
-        else if(n%2==0&&k%2==0)
-            a[i]=1;
-        else if(n%2==1&&k%2==1)
-            a[i]=1;
-        else if(n%2==0&&k%2==1&&n>2*k)
+        else if(k%2==1)
             a[i]=1;
         else
             a[i]=0;
     }
+
     for(i=0;i<t;i++){
         if(a[i]==1)
             printf("YES\n");
