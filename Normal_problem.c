71e88ae63edf2a07cc0461cb73b0f430ac30c4f6
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-18 20:39:14 +0600

    Update

--- Normal_problem.c
+++ Normal_problem.c
@@ -3,25 +3,28 @@
 int main()
 {
     int i,j,m=0,t;
-    char b[10000],c[100];
+    char b[10001],c[101];
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%s",c);
         a[i]=strlen(c);
-        for(j=0;j<a[i];j++,m++){
+        j=a[i];
+        while(j--){
             if(c[j]=='p')
                 b[m]='q';
             else if(c[j]=='q')
                 b[m]='p';
             else
                 b[m]=c[j];
+            m++;
         }
     }
     m=0;
-    for(i=0;i<t;i++){
+    for(i=0;i<t;i++)
+    {
         for(j=0;j<a[i];j++,m++)
-            printf("%c",b[m]);
+            printf("%c", b[m]);
         printf("\n");
     }
     return 0;
