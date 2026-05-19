fe6cbab0f72da22c0e0cfc638aa34f63ff813770
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-19 12:22:50 +0600

    Update

--- Creating_words.c
+++ Creating_words.c
@@ -1,12 +1,12 @@
-#include<Stdio.h>
+#include<stdio.h>
 int main()
 {
-    int i,t;
+    int i,j,t;
     scanf("%d",&t);
-    char a[t*7],b[t*7];
-    for(i=0;i<t*7;i+=7)
-    {
-        scanf("%s",a);
-    }
+    char a[t*6+1];
+    for(i=0,j=0;j<t;i+=6,j++)
+        scanf(" %c %c %c %c %c %c",&a[i],&a[i+1],&a[i+2],&a[i+3],&a[i+4],&a[i+5]);
+    for(i=0,j=0;j<t;i+=6,j++)
+        printf("%c%c%c %c%c%c\n",a[i+3],a[i+1],a[i+2],a[i],a[i+4],a[i+5]);
     return 0;
 }
