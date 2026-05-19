42a65a4d7d9129db3efff61aaf5dd9ff209b71b8
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-19 12:22:42 +0600

    Update

--- Short_substrings.c
+++ Short_substrings.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
 #include<string.h>
+
 int main()
 {
-    int c[1000],i,t;
-    char a[100],b[100];
+    int i,j,k,t;
+    char b[101],c[100001];
     scanf("%d",&t);
-    for(i=0;i<t;i++){
-        scanf("%s",b);
-        if(strlen(b)<3)
+    int a[t];
 
+    for(i=0,k=0;i<t;i++){
+        scanf("%s",b);
+        a[i]=strlen(b);
+        for(j=0;j<a[i];j+=2,k++)
+            c[k]=b[j];
+        c[k++]=b[j-1];
+        a[i]=a[i]/2+1;
     }
+
+    for(i=0,k=0;i<t;i++){
+        for(j=0;j<a[i];j++,k++)
+            printf("%c",c[k]);
+        printf("\n");
+    }
+
     return 0;
 }
