6b6148bcf6bf3366d6f26ea969965a4047a31f69
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-19 12:22:35 +0600

    Update

--- Cover_in_water.c
+++ Cover_in_water.c
@@ -1,31 +1,41 @@
 #include<stdio.h>
-#include<string.h>
+
 int main()
 {
-    int i,j,n,t,x,y;
+    int i,j,m,n,t;
     scanf("%d",&t);
     int a[t];
+
     for(i=0;i<t;i++){
         scanf("%d",&n);
         char b[n];
         scanf("%s",b);
-        x=0,y=0;
-        for(j=0;j<strlen(b);j++){
-            if(b[j]=='.'){
-                x++;
-                y++;
-            }
+        m=0;
+        a[i]=0;
+        for(j=0;j<n;j++){
+            if(b[j]=='.')
+                m++;
             else{
-                if(y>2){
+                if(m>2){
                     a[i]=2;
+                    n=0;
                     break;
                 }
-                y=0;
+                else
+                    a[i]+=m;
+                m=0;
             }
-            a[i]=x;
+        }
+        if(b[n-1]=='.'&&n!=0){
+            if(m>2)
+                a[i]=2;
+            else
+                a[i]+=m;
         }
     }
+
     for(i=0;i<t;i++)
         printf("%d\n",a[i]);
+
     return 0;
 }
