adbcab707c11fa5116e7c069bf55baeeffe0e139
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-20 13:26:05 +0600

    Update

--- Do_not_be_distracted.c
+++ Do_not_be_distracted.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n,t,x,y;
+    int i,j,m,n,t;
     scanf("%d",&t);
     int a[t];
     for(i=0;i<t;i++){
         scanf("%d",&n);
-        char b[n];
+        char b[n+1],x;
+        int c[26]={0};
         scanf("%s",b);
         a[i]=1;
-        for(j=0;j<n;j++){
-            for(k=j+2;k<n;j++){
-                if(b[k]==b[j]){
+        m=1;
+        c[b[0]-65]=1;;
+        for(j=1;j<n;j++){
+            if(b[j]!=b[j-1]){
+                if(c[b[j]-65]==0)
+                    c[b[j]-65]=1;
+                else{
                     a[i]=0;
                     break;
                 }
