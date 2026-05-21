9b38c2147d2a292a41fc02420810c600cddfe884
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-21 21:53:30 +0600

    Update

--- Forbiden_integer.c
+++ Forbiden_integer.c
@@ -4,6 +4,29 @@ int main()
     int i,k,n,t,x;
     scanf("%d",&t);
     int a[3*t];
+    for(i=0;i<3*t;i+=3){
+        scanf("%d %d %d",&n,&k,&x);
+        if(x!=1){
+            a[i]=0;
+            a[i+1]=n;
+            a[i+2]=1;
+        }
+        else if(n%2==0&&k>1){
+            a[i]=0;
+            a[i+1]=n/2;
+            a[i+2]=2;
+        }
+        else if(n%2==1&&k>2){
+            a[i]=3;
+            a[i+1]=n/2-1;
+            a[i+2]=2;
+        }
+        else{
+            a[i]=0;
+            a[i+1]=0;
+            a[i+2]=0;
+        }
+    }
 
     return 0;
 }
