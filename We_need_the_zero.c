5a6455826abedf6f0257949616b90e41ee76184f
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-02 16:59:53 +0600

    Update

--- We_need_the_zero.c
+++ We_need_the_zero.c
@@ -4,6 +4,12 @@ int main()
     int b,i,j,m,n,t;
     scanf("%d",&t);
     int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        m=0;
 
+    }
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
