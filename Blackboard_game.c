efcc8772ce35e7c94bc068dd163aef21e8442aac
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-05 20:36:15 +0600

    Update

--- Blackboard_game.c
+++ Blackboard_game.c
@@ -4,6 +4,13 @@ int main()
     int a,b,i,n,t;
     scanf("%d",&t);
     int c[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        if(n%4==0)
+            c[i]=1;
+        else
+            c[i]=0;
+    }
 
     return 0;
 }
