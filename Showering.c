5208680a2573a2b86e00c44a47c6504176448ad4
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-06 11:06:40 +0600

    Update

--- Showering.c
+++ Showering.c
@@ -4,7 +4,20 @@ int main()
     int i,j,m,n,s,t;
     scanf("%d",&t);
     int a[t];
-
+    for(i=0;i<t;i++){
+        scanf("%d %d %d",&n,&s,&m);
+        int b[2*n+2];
+        b[0]=a[i]=0;
+        for(j=1;j<=2*n;j+=2)
+            scanf("%d %d",&b[j],&b[j+1]);
+        b[2*n+1]=m;
+        for(j=0;j<2*n+2;j+=2){
+            if(s<=b[j+1]-b[j]){
+                a[i]=1;
+                break;
+            }
+        }
+    }
     for(i=0;i<t;i++){
         if(a[i])
             printf("YES\n");
