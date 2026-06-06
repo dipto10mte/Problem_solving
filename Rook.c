e3137a38ecd4b1803724076cdda5dd7930648cf4
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-06 11:06:47 +0600

    Update

--- Rook.c
+++ Rook.c
@@ -7,6 +7,17 @@ int main()
     char b[t];
     for(i=0;i<t;i++)
         scanf(" %c%d",&b[i],&a[i]);
-
+    for(i=0;i<t;i++){
+        for(j=97;j<105;j++){
+            if(j==b[i])
+                continue;
+            printf("%c%d\n",j,a[i]);
+        }
+        for(j=1;j<9;j++){
+            if(j==a[i])
+                continue;
+            printf("%c%d\n",b[i],j);
+        }
+    }
     return 0;
 }
