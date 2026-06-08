a1ec69fb7d2d254b44a61c9b6dac5055d5e9b2f6
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-08 11:35:47 +0600

    OddEven_increment

---
+++ OddEven_increments.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,l,m,n,o,p,t;
+    scanf("%d",&t);
+    int b[t];
+
+    for(i=0;i<t;i++){
+        if(b[i])
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
