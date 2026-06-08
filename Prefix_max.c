5348ebae06ed592caeffc4a67bcb9a8b57a2e19a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-08 11:35:14 +0600

    Update

--- Prefix_max.c
+++ Prefix_max.c
@@ -4,7 +4,16 @@ int main()
     int a,i,j,m,n,t;
     scanf("%d",&t);
     int b[t];
-
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        m=0;
+        for(j=0;j<n;j++){
+            scanf("%d",&a);
+            if(m<a)
+                m=a;
+            b[i]=m*n;
+        }
+    }
     for(i=0;i<t;i++)
         printf("%d\n",b[i]);
     return 0;
