ed5a0d7766ff209566ed7322a032df4f16c1deb2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-09 11:34:41 +0600

    Update

--- Be_positive.c
+++ Be_positive.c
@@ -4,7 +4,20 @@ int main()
     int a,i,j,n,o,p,t;
     scanf("%d",&t);
     int b[t];
-
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        o=p=0;
+        for(j=0;j<n;j++){
+            scanf("%d",&a);
+            if(a==0)
+                o++;
+            else if(a==-1)
+                p++;
+        }
+        b[i]=o;
+        if(p%2==1)
+            b[i]+=2;
+    }
     for(i=0;i<t;i++)
         printf("%d\n",b[i]);
     return 0;
