8fe0c0215d497f696f4e4bc446bca70aee5d7cf2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-10 10:26:28 +0600

    Update

--- Sakurako_and_kosuke.c
+++ Sakurako_and_kosuke.c
@@ -4,7 +4,13 @@ int main()
     int i,n,t;
     scanf("%d",&t);
     int a[t];
-
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        if(n%2==1)
+            a[i]=0;
+        else
+            a[i]=1;
+    }
     for(i=0;i<t;i++){
         if(a[i])
             printf("Sakurako\n");
