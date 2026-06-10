f581498e807914a22e62bbf7fc2b37c05a92da4a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-10 10:26:19 +0600

    Update

--- Arithmetic_array.c
+++ Arithmetic_array.c
@@ -4,7 +4,20 @@ int main()
     int a,i,j,m,n,t;
     scanf("%d",&t);
     int b[t];
-
+    for(i=0;i<t;i++){
+        scanf("%d",&n);
+        m=0;
+        for(j=0;j<n;j++){
+            scanf("%d",&a);
+            m+=a;
+        }
+        if(m<n)
+            b[i]=1;
+        else if(m==n)
+            b[i]=0;
+        else
+            b[i]=m-n;
+    }
     for(i=0;i<t;i++)
         printf("%d\n",b[i]);
     return 0;
