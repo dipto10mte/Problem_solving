38f989afdd825397a563b7e3c38a6f970cdbdb94
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-11 10:28:24 +0600

    Update

--- Polycarp_and_the_day_of_pi.c
+++ Polycarp_and_the_day_of_pi.c
@@ -3,9 +3,17 @@
 int main()
 {
     int i,j,t;
-    char m[31]={'3141592653589793238462643383279'},n[31];
+    char m[31]="3141592653589793238462643383279",n[31];
     scanf("%d",&t);
     int a[t];
+    for(i=0;i<t;i++){
+        scanf("%s",n);
+        for(j=0;j<strlen(n);j++){
+            if(m[j]!=n[j])
+                break;
+        }
+        a[i]=j;
+    }
     for(i=0;i<t;i++)
         printf("%d\n",a[i]);
     return 0;
