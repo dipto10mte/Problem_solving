81980a579f1eac6d279c1a2d2f4ef545f15d1fc1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-11 10:23:53 +0600

    Polycarp_and_the_day_of_pi

---
+++ Polycarp_and_the_day_of_pi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,t;
+    char m[31]={'3141592653589793238462643383279'},n[31];
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
