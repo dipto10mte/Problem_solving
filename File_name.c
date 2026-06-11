3b08b12710346ff17415648bf96695620ca06e24
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-11 10:37:54 +0600

    Update

--- File_name.c
+++ File_name.c
@@ -2,11 +2,25 @@
 #include<string.h>
 int main()
 {
-    int i,j,l=0,m=0;
+    int i,j,l=0,m=0,n;
     scanf("%d",&n);
     char a[n+1];
     scanf("%s",a);
-
+    for(i=0;i<n;i++){
+        if(a[i]=='x')
+            l++;
+        else{
+            if(l>2){
+                l-=2;
+                m+=l;
+            }
+            l=0;
+        }
+    }
+    if(l>2){
+        l-=2;
+        m+=l;
+    }
     printf("%d",m);
     return 0;
 }
