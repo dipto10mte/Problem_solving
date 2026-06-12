bb2a4867aa98a6ea2a72b19f5d92568f8670946a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-12 14:46:46 +0600

    Update

--- Ideal_generator.c
+++ Ideal_generator.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int
+    int i,k,t;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++){
+        scanf("%d",&k);
+        if(k%2==1)
+            a[i]=1;
+        else
+            a[i]=0;
+    }
+    for(i=0;i<t;i++){
+        if(a[i])
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
     return 0;
 }
