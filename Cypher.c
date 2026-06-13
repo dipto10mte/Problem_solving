d1edbb65ae8ae5ee217957655ff7e1baac15a322
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-13 09:48:03 +0600

    Update

--- Cypher.c
+++ Cypher.c
@@ -12,6 +12,19 @@ int main() {
         for (int i = 0; i < n[tc]; i++) scanf("%d", &a[tc][i]);
         for (int i = 0; i < n[tc]; i++) scanf("%d %s", &b[tc][i], moves[tc][i]);
     }
-
+    for (int tc = 0; tc < t; tc++) {
+        for (int i = 0; i < n[tc]; i++) {
+            int digit = a[tc][i];
+            for (int j = 0; j < b[tc][i]; j++) {
+                if
+                    (moves[tc][i][j] == 'U') digit = (digit - 1 + 10) % 10;
+                else
+                    digit = (digit + 1) % 10;
+            }
+            printf("%d", digit);
+            if (i < n[tc] - 1) printf(" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
