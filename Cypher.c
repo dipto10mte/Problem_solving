cd26b2a8045f54127affbce52984ebd40fbc8de5
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-13 09:47:32 +0600

    Update

--- Cypher.c
+++ Cypher.c
@@ -7,6 +7,11 @@ int main() {
     int a[105][105];
     int b[105][105];
     char moves[105][105][15];
+    for (int tc = 0; tc < t; tc++) {
+        scanf("%d", &n[tc]);
+        for (int i = 0; i < n[tc]; i++) scanf("%d", &a[tc][i]);
+        for (int i = 0; i < n[tc]; i++) scanf("%d %s", &b[tc][i], moves[tc][i]);
+    }
 
     return 0;
 }
