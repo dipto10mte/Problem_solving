7eb0b694fdb193783f3402621eeb5880c4207992
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-14 10:09:34 +0600

    Update

--- New_world_new_me_new_array.c
+++ New_world_new_me_new_array.c
@@ -5,6 +5,13 @@ int main() {
     int n[1005],k[1005],p[1005];
     for (int tc=0;tc<t;tc++)
         scanf("%d %d %d",&n[tc],&k[tc],&p[tc]);
-
+    for (int tc=0;tc<t;tc++) {
+        int maxSum=n[tc]*p[tc];
+        int absK=k[tc]<0?-k[tc]:k[tc];
+        if (absK > maxSum)
+            printf("-1\n");
+        else
+            printf("%d\n",(absK+p[tc]-1)/p[tc]);
+    }
     return 0;
 }
