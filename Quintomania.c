dd2d906b05bef607ec01949e9234573ea2256991
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-15 19:46:53 +0600

    Update

--- Quintomania.c
+++ Quintomania.c
@@ -10,7 +10,17 @@ int main() {
         for (int i = 0; i < n; i++)
             scanf("%d", &a[i]);
         int ok = 1;
-
+        for (int i = 1; i < n; i++) {
+            int diff = abs(a[i] - a[i - 1]);
+            if (diff != 5 && diff != 7) {
+                ok = 0;
+                break;
+            }
+        }
+        if (ok)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
