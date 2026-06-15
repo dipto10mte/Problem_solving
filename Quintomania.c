d760f3fa30dd407ba0934b463c1ca9f4f7910547
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-15 19:46:32 +0600

    Update

--- Quintomania.c
+++ Quintomania.c
@@ -3,6 +3,14 @@
 int main() {
     int t;
     scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i < n; i++)
+            scanf("%d", &a[i]);
+        int ok = 1;
 
+    }
     return 0;
 }
