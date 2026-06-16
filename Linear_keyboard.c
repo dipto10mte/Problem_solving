6cddc488ee620c1c5a884770539fad68127b9a73
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-16 15:19:05 +0600

    Update

--- Linear_keyboard.c
+++ Linear_keyboard.c
@@ -1,6 +1,12 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
     return 0;
 }
