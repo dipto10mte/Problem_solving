d0207cc270de5f92235724c6049f5cf2a44d535f
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-16 15:19:22 +0600

    Update

--- Linear_keyboard.c
+++ Linear_keyboard.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void solve() {
+    char keyboard[27], word[200001];
+    scanf("%s %s", keyboard, word);
+    int pos[26];
+    for(int i=0;i<26;i++) {
+        pos[keyboard[i] - 'a'] = i;
+    }
 
+}
 int main() {
     int t;
     scanf("%d", &t);
