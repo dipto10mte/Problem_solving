a7d7c4fbab1c6964501ae4897a72695db3c841bf
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-17 14:50:00 +0600

    Update

--- Stone_game.c
+++ Stone_game.c
@@ -3,6 +3,15 @@ int main() {
     int t;
     scanf("%d", &t);
     while (t--) {
+        int n;
+        scanf("%d", &n);
+        int a[105];
+        int min_idx = 0, max_idx = 0;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+            if (a[i] < a[min_idx]) min_idx = i;
+            if (a[i] > a[max_idx]) max_idx = i;
+        }
 
     }
     return 0;
