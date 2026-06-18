1aae2fa0a776336610d6155563e0616ecfb4c72b
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-18 13:03:21 +0600

    Update

--- Journey.c
+++ Journey.c
@@ -3,6 +3,15 @@ int main()
 {
     int t;
     scanf("%d", &t);
+    while (t--) {
+        long long n, a, b, c;
+        scanf("%lld %lld %lld %lld", &n, &a, &b, &c);
+        long long cycle = a + b + c;
+        long long full_cycles = (n - 1) / cycle;
+        long long total = full_cycles * cycle;
+        long long days = full_cycles * 3;
+        long long rem[3] = {a, b, c};
 
+    }
     return 0;
 }
