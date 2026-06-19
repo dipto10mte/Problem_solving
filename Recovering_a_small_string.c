7f2e4ac498422616e9b8f08320e2343d9cec7060
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-19 12:00:25 +0600

    Update

--- Recovering_a_small_string.c
+++ Recovering_a_small_string.c
@@ -6,7 +6,18 @@ int main(void) {
         int n;
         scanf("%d",&n);
         char ans[4];
-
+        for (int a=1;a<=26;a++) {
+            for (int b=1;b<=26;b++) {
+                int c=n-a-b;
+                if (c>=1&&c<=26){
+                    ans[0]=(char)('a'+a-1);
+                    ans[1]=(char)('a'+b-1);
+                    ans[2]=(char)('a'+c-1);
+                    ans[3]='\0';
+                    goto found;
+                }
+            }
+        }
         found:
         printf("%s\n", ans);
     }
