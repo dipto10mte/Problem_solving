c38ff5f0824223020fcbe88cc031579d9174618d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-19 12:00:11 +0600

    Update

--- Recovering_a_small_string.c
+++ Recovering_a_small_string.c
@@ -3,7 +3,12 @@ int main(void) {
     int t;
     scanf("%d",&t);
     while(t--) {
+        int n;
+        scanf("%d",&n);
+        char ans[4];
 
+        found:
+        printf("%s\n", ans);
     }
     return 0;
 }
