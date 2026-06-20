cbaaa8ee9487c2b551c7ff8723688e4dbd067785
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-20 14:57:47 +0600

    Update

--- Sum_in_binary_tree.c
+++ Sum_in_binary_tree.c
@@ -6,7 +6,11 @@ int main(){
         long long n;
         scanf("%lld",&n);
         long long sum=0;
-
+        while (n>=1) {
+            sum+=n;
+            n/=2;
+        }
+        printf("%lld\n",sum);
     }
     return 0;
 }
