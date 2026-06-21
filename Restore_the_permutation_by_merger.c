4d831a5b91759f8bfa6c3740901081b3bd8fefff
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-21 10:53:20 +0600

    Update

--- Restore_the_permutation_by_merger.c
+++ Restore_the_permutation_by_merger.c
@@ -10,7 +10,15 @@ int main(){
         for(int i=0;i<2*n;i++)
             scanf("%d", &a[i]);
         int p[55],idx=0;
-
+        for (int i=0;i<2*n;i++) {
+            if (!seen[a[i]]){
+                seen[a[i]]=1;
+                p[idx++]=a[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            printf("%d%c",p[i],i+1==n?'\n':' ');
+        }
     }
     return 0;
 }
