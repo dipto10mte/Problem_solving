4f33ccba58414f5a8975425bebe0476f0adca385
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-23 15:06:43 +0600

    Update

--- Games_on_the_train.c
+++ Games_on_the_train.c
@@ -3,7 +3,11 @@ int main(){
     int t;
     scanf("%d",&t);
     while (t--){
+        int n;
+        scanf("%d",&n);
+        int mn=1e9,mx=0;
 
+        printf("%d\n", mx - mn + 1);
     }
     return 0;
 }
