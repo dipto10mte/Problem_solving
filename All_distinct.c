474f7975f8a9c96ba6d6ce639371c067aa0d7668
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-24 14:28:17 +0600

    Update

--- All_distinct.c
+++ All_distinct.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+int cmp(const void *a,const void *b) {
+    return (*(int*)a -*(int*)b);
 }
+
 int main() {
     int t;
     scanf("%d",&t);
