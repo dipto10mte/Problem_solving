370604efe35e788b3795c487a3f9b57850a087a9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-24 14:28:30 +0600

    Update

--- All_distinct.c
+++ All_distinct.c
@@ -4,7 +4,15 @@
 int cmp(const void *a,const void *b) {
     return (*(int*)a -*(int*)b);
 }
+void solve() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    qsort(a,n,sizeof(int),cmp);
 
+}
 int main() {
     int t;
     scanf("%d",&t);
