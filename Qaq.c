4771a7e908de7f47f59e944fd77d6a358d3da205
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-26 12:41:30 +0600

    Update

--- Qaq.c
+++ Qaq.c
@@ -8,7 +8,14 @@ int main(){
     for(int i=0;i<n;i++){
         if(s[i]=='A') {
             long long left=0,right=0;
-
+            for(int j=0;j<i;j++)
+                if(s[j]=='Q')
+                    left++;
+            for(int j=i+1;j<n;j++)
+                if(s[j]=='Q')
+                    right++;
+            ans+=left*right;
+        }
     }
     printf("%lld\n",ans);
     return 0;
