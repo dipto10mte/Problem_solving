1c1597bc282b3926c3c25af8102cdaaa845b56eb
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-29 01:53:13 +0600

    Update

--- Lever.c
+++ Lever.c
@@ -2,6 +2,15 @@
 int main(){
     int t;
     scanf("%d",&t);
+    while(t--){
+        int n;
+        scanf("%d",&n);
+        long long a[200005],b[200005];
+        for (int i=0;i<n;i++)
+            scanf("%lld",&a[i]);
+        for (int i=0;i<n;i++)
+        scanf("%lld",&b[i]);
 
+    }
     return 0;
 }
