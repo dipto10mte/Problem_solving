d77feedbc741c91cd74e7a3f19dcd8bdb1ca7893
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-06-30 14:39:35 +0600

    Update

--- Vasya_and_coins.c
+++ Vasya_and_coins.c
@@ -6,7 +6,11 @@ int main(){
         long long a,b;
         scanf("%lld %lld",&a,&b);
         long long ans;
-
+        if(a==0)
+            ans=1;
+        else
+            ans=a+2*b+1;
+        printf("%lld\n",ans);
     }
     return 0;
 }
