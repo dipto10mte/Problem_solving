599295091ae003b5b0464a29288dd057e9bca390
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-01 14:47:45 +0600

    Update

--- Olympiad_date.c
+++ Olympiad_date.c
@@ -2,7 +2,14 @@
 int main(){
     int t;
     scanf("%d",&t);
+    while (t--){
+        int n;
+        scanf("%d",&n);
+        int cnt[10]={};
+        int ans=0;
 
+        }
+        printf("%d\n",ans);
     }
     return 0;
 }
