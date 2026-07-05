3d57a25e89b8f45de13b53147266f08c8a7b1445
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-05 11:03:45 +0600

    Update

--- FashionabLee.c
+++ FashionabLee.c
@@ -5,7 +5,10 @@ int main(){
     while(t--){
         long long n;
         scanf("%lld",&n);
-
+        if(n%4==0)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
