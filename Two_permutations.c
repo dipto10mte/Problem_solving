a3e19b7ff36ce34005315f1fd433a8b3446b33b3
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-06 15:37:33 +0600

    Update

--- Two_permutations.c
+++ Two_permutations.c
@@ -6,7 +6,11 @@ int main(){
         int n,a,b;
         scanf("%d %d %d",&n,&a,&b);
         int ans;
-
+        if(a==n||b==n)
+            ans=(a==n&&b==n);
+        else
+            ans=(a+b<=n-2);
+        printf(ans?"Yes\n":"No\n");
     }
     return 0;
 }
