31159611450d2017c3b41d240c6254139ef94d35
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-07 15:17:48 +0600

    Update

--- Who's_opposite.c
+++ Who's_opposite.c
@@ -7,7 +7,13 @@ int main(){
         long long a,b,c;
         scanf("%lld %lld %lld",&a,&b,&c);
         long long n=2*llabs(a-b);
-
+        if (a>n||b>n||c>n)
+            printf("-1\n");
+        else{
+            long long d=c+n/2;
+            while(d>n)d-=n;
+            printf("%lld\n",d);
+        }
     }
     return 0;
 }
