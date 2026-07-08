e586602a5140ba7cf80ed4544ac7a58af0f2ccb6
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-08 19:53:35 +0600

    Update

--- Where's_the_bishop.c
+++ Where's_the_bishop.c
@@ -4,7 +4,15 @@ void solve(){
     for(int i=0;i<8;i++)
             scanf("%s",board[i]);
     for(int i=1;i<7;i++) {
-
+        for(int j=1;j<7;j++){
+            if (board[i][j]=='#'&&
+                board[i-1][j-1]=='#'&&
+                board[i-1][j+1]=='#'&&
+                board[i+1][j-1]=='#'&&
+                board[i+1][j+1]=='#') {
+                printf("%d %d\n", i + 1, j + 1);
+                return;
+            }
         }
     }
 }
