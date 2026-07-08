bb01c9dbfb4a7ec702d8bc15f0f53f38ae201557
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-08 19:53:21 +0600

    Update

--- Where's_the_bishop.c
+++ Where's_the_bishop.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 void solve(){
     char board[8][9];
+    for(int i=0;i<8;i++)
+            scanf("%s",board[i]);
+    for(int i=1;i<7;i++) {
 
+        }
+    }
 }
 int main(){
     int t;
