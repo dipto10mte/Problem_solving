499f7dc069d5c564902622863a1cc9236c0b99ee
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-09 11:12:01 +0600

    Update

--- Copycopycopycopycopy.c
+++ Copycopycopycopycopy.c
@@ -3,7 +3,15 @@
 int compare(const void *a,const void *b){
     return (*(int*)a-*(int*)b);
 }
+void solve(){
+    int n;
+    scanf("%d",&n);
+    int *a=(int*)malloc(n*sizeof(int));
+    for (int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    qsort(a,n,sizeof(int),compare);
 
+}
 int main(){
     int t;
     scanf("%d",&t);
