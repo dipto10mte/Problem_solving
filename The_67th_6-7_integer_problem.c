5a3c1d05a96589297de4eefedc941b17e660b7df
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-10 12:20:38 +0600

    Update

--- The_67th_6-7_integer_problem.c
+++ The_67th_6-7_integer_problem.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+int compare(const void *a,const void *b) {
+    long long x=*(const long long *)a;
+    long long y=*(const long long *)b;
+    if(x<y)
+        return -1;
+    if(x>y)
+        return 1;
+    return 0;
+}
 
 int main(){
     int t;
