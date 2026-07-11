e8bda7403e860ea0891a02e691db1dcf5ca625b7
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-11 09:52:14 +0600

    Update

--- Choosing_cubes.c
+++ Choosing_cubes.c
@@ -3,7 +3,19 @@
 int compare_desc(const void *a,const void *b){
     return (*(int*)b-*(int*)a);
 }
+void solve(){
+    int n,f,k;
+    if(scanf("%d %d %d",&n,&f,&k)!=3)
+        return;
+    int *a=(int*)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int fav_value=a[f -1];
+    qsort(a,n,sizeof(int),compare_desc);
+    int first_pos=-1;
+    int last_pos=-1;
 
+}
 int main(){
     int t;
     if (scanf("%d",&t)==1){
