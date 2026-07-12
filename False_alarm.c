96739a13cdd33b77b6d3a730fd0507622621a3d6
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-12 10:42:21 +0600

    Update

--- False_alarm.c
+++ False_alarm.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+void solve(){
+    int n,x;
+    if (scanf("%d %d",&n,&x)!=2)
+        return;
+    int first_i=-1;
+    int last_i=-1;
 
+}
 int main(){
     int t;
     if (scanf("%d",&t)==1){
