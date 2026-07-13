a791a43416678688f96e9a621489d8af75a47a8c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-13 09:39:35 +0600

    Update

--- Cipher_sifer.c
+++ Cipher_sifer.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+void solve(){
+    int n;
+    scanf("%d",&n);
+    char s[105];
+    scanf("%s",s);
 
+    printf("\n");
+}
 int main(){
     int t;
     scanf("%d",&t);
