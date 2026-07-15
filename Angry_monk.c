ba37ecb422e5f5ac68cf1b4b663456f3bf01dda7
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-15 12:20:59 +0600

    Update

--- Angry_monk.c
+++ Angry_monk.c
@@ -6,7 +6,11 @@ void solve(){
     int max_val=0;
     long long total_ops=0;
     for (int i=0;i<k;i++){
-
+        int a;
+        scanf("%d",&a);
+        if (a>max_val)
+            max_val=a;
+        total_ops+=(2LL*a-1);
     }
     total_ops-=(2LL*max_val-1);
     printf("%lld\n",total_ops);
