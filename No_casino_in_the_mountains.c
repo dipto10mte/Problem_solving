6399b607460abbf84b6994a424886cb624708b0e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-16 15:35:20 +0600

    Update

--- No_casino_in_the_mountains.c
+++ No_casino_in_the_mountains.c
@@ -3,6 +3,16 @@
 int a[MAXN];
 int pref[MAXN];
 void solve(){
+    int n,k;
+    if(scanf("%d %d",&n,&k)!=2)
+        return;
+    pref[0]=0;
+    for(int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+        pref[i+1]=pref[i]+a[i];
+    }
+    int ans=0;
+    int i=0;
 
 }
 int main(){
