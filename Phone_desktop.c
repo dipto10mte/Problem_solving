d3650115ff11d8e88249082772837f40a8b3b21e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-17 21:15:07 +0600

    Update

--- Phone_desktop.c
+++ Phone_desktop.c
@@ -2,7 +2,10 @@
 void solve(){
     int x,y;
     scanf("%d %d",&x,&y);
+    int screens=(y+1)/2;
+    int remaining_cells=(screens*15)-(y*4);
 
+    printf("%d\n",screens);
 }
 int main(){
     int t;
