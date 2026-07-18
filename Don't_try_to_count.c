a4d7f19e0cac38fd7a6a13186741683a071631d9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-18 19:31:04 +0600

    Update

--- Don't_try_to_count.c
+++ Don't_try_to_count.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
-int main()
-{
-    int i,m,n,t;
-    scanf("%d",&t);
-    int a[t];
-    for(i=0;i<t;i+=2){
-        scanf("%d %d",&n,&m);
-        char x[n],s[m];
+#include<string.h>
+
+int main() {
+    int t;
+    if (scanf("%d",&t)==1) {
+        while(t--)
+            solve();
     }
-    for(i=0;i<t;i+=2)
-        printf("%d\n",a[i]);
     return 0;
 }
