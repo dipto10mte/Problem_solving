e136e1563936c45aed72134e423f7068acb4c2d0
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-18 19:31:33 +0600

    Update

--- Don't_try_to_count.c
+++ Don't_try_to_count.c
@@ -3,7 +3,13 @@
 void solve(){
     int n,m;
     scanf("%d %d",&n,&m);
+    char x[2000];
+    char s[30];
+    scanf("%s",x);
+    scanf("%s",s);
+    int ops=0;
 
+    printf("-1\n");
 }
 int main() {
     int t;
