925ef1b6adcb0d69197ae25872c3c6c25f3926b0
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-18 19:31:43 +0600

    Update

--- Don't_try_to_count.c
+++ Don't_try_to_count.c
@@ -8,7 +8,17 @@ void solve(){
     scanf("%s",x);
     scanf("%s",s);
     int ops=0;
-
+    while(ops<=6){
+        if(strstr(x,s)!=NULL){
+            printf("%d\n",ops);
+            return;
+        }
+        int current_len=strlen(x);
+        if (current_len*2>=sizeof(x))
+            break;
+        strcpy(x+current_len,x);
+        ops++;
+    }
     printf("-1\n");
 }
 int main() {
