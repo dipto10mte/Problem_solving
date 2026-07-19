0b0db1c554722065cadd19ba6448cf212e76f793
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-19 14:26:23 +0600

    Update

--- Night_at_the_museum.c
+++ Night_at_the_museum.c
@@ -7,7 +7,12 @@ int main() {
         return 0;
     int total_moves=0;
     char current='a';
-
+    for (int i=0;s[i]!='\0';i++) {
+        int diff=abs(s[i]-current);
+        int moves=diff<(26-diff)? diff:(26-diff);
+        total_moves+=moves;
+        current=s[i];
+    }
     printf("%d\n",total_moves);
     return 0;
 }
