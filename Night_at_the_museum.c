4f067cebebdea5209163d92c898b4cdcbdb711f5
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-19 14:26:11 +0600

    Update

--- Night_at_the_museum.c
+++ Night_at_the_museum.c
@@ -5,7 +5,10 @@ int main() {
     char s[101];
     if (scanf("%100s", s)!= 1)
         return 0;
+    int total_moves=0;
+    char current='a';
 
+    printf("%d\n",total_moves);
     return 0;
 }
 
