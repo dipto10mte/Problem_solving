a9fa2b7c40300a32fd8b28630e3592871262cab2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-21 00:36:45 +0600

    Update

--- LCM_problem.c
+++ LCM_problem.c
@@ -2,7 +2,10 @@
 void solve(){
     long long l,r;
     scanf("%lld %lld",&l,&r);
-
+    if(2*l<=r)
+        printf("%lld %lld\n",l,2*l);
+    else
+        printf("-1 -1\n");
 }
 int main(){
     int t;
