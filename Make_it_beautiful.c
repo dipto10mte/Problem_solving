351347698533640ecdd46b5991092f353d3c4217
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-20 10:12:34 +0600

    Update

--- Make_it_beautiful.c
+++ Make_it_beautiful.c
@@ -2,7 +2,15 @@
 int compare(const void *a,const void *b){
     return (*(int*)b-*(int*)a);
 }
+void solve(){
+    int n;
+    scanf("%d",&n);
+    int a[50];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    qsort(a,n,sizeof(int),compare);
 
+}
 int main(){
     int t;
     scanf("%d",&t);
