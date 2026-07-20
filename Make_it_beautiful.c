bacdab0ec61fded675a4ed7fbdb8e97f6628e083
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-20 10:12:01 +0600

    Update

--- Make_it_beautiful.c
+++ Make_it_beautiful.c
@@ -1,56 +1,10 @@
 #include<stdio.h>
 
-int sort(int arr[],int n){
-    int i,j,m=0,temp;
-    for(i=1;i<n;i++){
-        if(arr[i]>arr[m]){
-            m=i;
-        }
-    }
-    if(m!=0){
-        temp=arr[0];
-        arr[0]=arr[m];
-        arr[m]=temp;
-    }
-    for(i=1;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
-    return arr[n];
-}
-int main()
-{
-    int i,j,m,n,t;
+int main(){
+    int t;
     scanf("%d",&t);
-    int a[t];
-    for(i=0;i<t;i++){
-        scanf("%d",&n);
-        int b[n];
-        m=0;
-        for(j=0;j<n;j++)
-            scanf("%d",&b[j]);
-        for(j=1;j<n;j++){
-            if(b[j-1]==b[j])
-                m++;
-        }
-        if(m==n-1)
-            a[i]=0;
-        else{
-            a[i]=1;
-        }
-    }
-    for(i=0;i<t;i++){
-        if(a[i]==1){
-            printf("YES\n");
-        }
-        else
-            printf("NO\n");
-    }
+    while(t--)
+        solve();
     return 0;
 }
 
