081967b725a4ac78b18397e6960ae4423dadbade
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-22 08:18:39 +0600

    Update

--- Twin_permutation.c
+++ Twin_permutation.c
@@ -2,7 +2,11 @@
 void solve(){
     int n;
     scanf("%d",&n);
-
+    for(int i=0;i<n;i++){
+        int a_i;
+        scanf("%d",&a_i);
+        printf("%d%c",(n+1)-a_i,(i==n-1)?'\n':' ');
+    }
 }
 int main(){
     int t;
