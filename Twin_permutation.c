32f6409b950bf883ca850549b2f7c0789d7c3ec2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-22 08:17:46 +0600

    Update

--- Twin_permutation.c
+++ Twin_permutation.c
@@ -1,27 +1,10 @@
 #include<stdio.h>
-int main()
-{
-    int b[200000],i,j,k=0,m,n,t;
-    scanf("%d",&t);
-    int a[t];
-    for(i=0;i<t;i++){
-        scanf("%d",&n);
-        int c[n];
-        a[i]=n;
-        for(j=0;j<n;j++)
-            scanf("%d",&c[j]);
-        b[k++]=1;
-        for(j=1;j<n;j++,k++){
-            if(c[j-1]+b[k-1]>c[j])
-                b[k]=c[j-1]+b[k-1]-c[j]+1;
-            else
-                b[k]=1;
-        }
-    }
-    for(i=0,k=0;i<t;i++){
-        for(j=0;j<a[i];j++,k++)
-            printf("%d ",b[k]);
-        printf("\n");
-    }
+
+int main(){
+    int t;
+    if(scanf("%d",&t)!=1)
+        return 0;
+    while (t--)
+        solve();
     return 0;
 }
