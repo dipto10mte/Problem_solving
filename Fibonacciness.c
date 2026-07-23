5c564f14d004012b6b245389d8a54bfb72fdd7bc
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-23 09:42:04 +0600

    Update

--- Fibonacciness.c
+++ Fibonacciness.c
@@ -12,7 +12,15 @@ int count_fibonacciness(int a1,int a2,int a3,int a4,int a5){
         count++;
     return count;
 }
+void solve(){
+    int a1,a2,a4,a5;
+    scanf("%d %d %d %d",&a1,&a2,&a4,&a5);
+    int cand1=a1+a2;
+    int cand2=a4-a2;
+    int cand3=a5-a4;
 
+
+}
 int main(){
     int t;
     if(scanf("%d",&t)!=1)
