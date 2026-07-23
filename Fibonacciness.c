5b819da796753359edd9403cd5c906301f554951
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-23 09:41:50 +0600

    Update

--- Fibonacciness.c
+++ Fibonacciness.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
+int max(int a,int b) {
+    return (a>b)?a:b;
+}
+int count_fibonacciness(int a1,int a2,int a3,int a4,int a5){
+    int count = 0;
+    if(a1+a2==a3)
+        count++;
+    if(a2+a3==a4)
+        count++;
+    if(a3+a4==a5)
+        count++;
+    return count;
+}
 
 int main(){
     int t;
