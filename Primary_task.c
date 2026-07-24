e2ebb63398b0f574d56549481bf2c192654ad4ed
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-24 12:28:52 +0600

    Update

--- Primary_task.c
+++ Primary_task.c
@@ -4,7 +4,12 @@ void solve(){
     char s[20];
     scanf("%s",s);
     int len=strlen(s);
-
+    if(len>=3&&s[0]=='1'&&s[1]=='0'&&s[2]!='0'){
+        if(len==3&&s[2]=='1')
+            printf("NO\n");
+        else
+            printf("YES\n");
+    }
     else
         printf("NO\n");
 }
