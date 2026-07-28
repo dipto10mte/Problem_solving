dd747efe01bcceda8920f81e4d76110cab495353
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-28 09:18:06 +0600

    Update

--- Sasha_and_array_coloring.c
+++ Sasha_and_array_coloring.c
@@ -3,7 +3,15 @@
 int compare(const void *a,const void *b){
     return(*(int*)a-*(int*)b);
 }
+void solve(){
+    int n;
+    if (scanf("%d",&n)!=1)
+        return;
+    int a[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
