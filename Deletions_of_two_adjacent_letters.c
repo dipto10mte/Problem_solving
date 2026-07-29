da393e4d2c683f8358c106b32c6f2b96d0e95d16
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-29 14:36:52 +0600

    Update

--- Deletions_of_two_adjacent_letters.c
+++ Deletions_of_two_adjacent_letters.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
+void solve(){
+    char s[105];
+    char c;
+    scanf("%s",s);
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
