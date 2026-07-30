afc4570326a8d6687be68261dcb37c60749edbce
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-30 09:20:59 +0600

    Update

--- I'm_bored_with_life.c
+++ I'm_bored_with_life.c
@@ -5,6 +5,8 @@ int main(){
         return 0;
     long long min_val=(a<b)?a:b;
     long long factorial=1;
-
+    for(long long i=1;i<=min_val;i++)
+        factorial*=i;
+    printf("%lld\n",factorial);
     return 0;
 }
