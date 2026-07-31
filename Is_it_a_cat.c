64d4c0103499fa45bfb5c8b276d53e379d992f00
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-31 12:22:15 +0600

    Update

--- Is_it_a_cat.c
+++ Is_it_a_cat.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
+void solve(){
+    int n;
+    scanf("%d",&n);
+    char s[55];
+    scanf("%s",s);
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
