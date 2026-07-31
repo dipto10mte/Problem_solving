038af66fa3d8f17f251595b16b12fa4dbb767a6c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-07-31 12:22:55 +0600

    Update

--- Is_it_a_cat.c
+++ Is_it_a_cat.c
@@ -8,8 +8,12 @@ void solve(){
     scanf("%s",s);
     char compressed[55];
     int len=0;
-
-
+    for(int i=0;i<n;i++){
+        char c=tolower((unsigned char)s[i]);
+        if(len==0||compressed[len-1]!=c)
+            compressed[len++] =c;
+    }
+    compressed[len]='\0';
     if(strcmp(compressed,"meow")==0)
         printf("YES\n");
     else
