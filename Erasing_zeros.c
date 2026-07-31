2c4675585d188b9837b5e4c2e86f58c6b171610c
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-01 03:43:11 +0600

    Update

--- Erasing_zeros.c
+++ Erasing_zeros.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<string.h>
+void solve(){
+    char s[105];
+    if(scanf("%s",s)!=1)
+        return;
+    int len=strlen(s);
+    int first=-1,last=-1;
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
