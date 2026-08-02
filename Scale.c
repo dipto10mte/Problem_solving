2e06ff391e503d8285ce5a0bb1d183fe474c7754
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-02 14:27:30 +0600

    Update

--- Scale.c
+++ Scale.c
@@ -5,7 +5,11 @@ void solve(){
     char grid[1005][1005];
     for(int i=0;i<n;i++)
         scanf("%s", grid[i]);
-
+    for(int i=0;i<n;i+=k){
+        for(int j=0;j<n;j+=k)
+            putchar(grid[i][j]);
+        putchar('\n');
+    }
 }
 int main(){
     int t;
