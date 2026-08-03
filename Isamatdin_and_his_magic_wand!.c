e719f9d1b3b0c090244546bc8c66cc46af27d901
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-03 09:54:34 +0600

    Update

--- Isamatdin_and_his_magic_wand!.c
+++ Isamatdin_and_his_magic_wand!.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUF_SZ (1 << 25)
+
+static char ibuf[BUF_SZ];
+static int ipos = 0, ilen = 0;
+
+static inline int gc(void) {
+    if (ipos == ilen) {
+        ilen = (int)fread(ibuf, 1, BUF_SZ, stdin);
+        ipos = 0;
+        if (ilen == 0) return -1;
+    }
+    return ibuf[ipos++];
+}
+
+static inline long long readLL(void) {
+    int c = gc();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = gc();
+    int neg = 0;
+    if (c == '-') { neg = 1; c = gc(); }
+    long long x = 0;
+    while (c >= '0' && c <= '9') {
+        x = x * 10 + (c - '0');
+        c = gc();
+    }
+    return neg ? -x : x;
+}
+
+
 
 int main() {
     long long t = readLL();
