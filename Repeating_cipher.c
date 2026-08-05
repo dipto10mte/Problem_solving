4a33d32637076b9aeb91af098de61b779550996e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-05 11:25:25 +0600

    Update

--- Repeating_cipher.c
+++ Repeating_cipher.c
@@ -7,6 +7,11 @@ int main(){
     scanf("%s",s);
     int index=0;
     int step=1;
-
+    while(index<n){
+        putchar(s[index]);
+        index+=step;
+        step++;
+    }
+    putchar('\n');
     return 0;
 }
