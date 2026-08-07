b126304ddc7c83f768d1d4b1e62da2148d7b5cc9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-07 10:36:11 +0600

    Update

--- Longest_good_array.c
+++ Longest_good_array.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<math.h>
+void solve(){
+    long long l,r;
+    if(scanf("%lld %lld",&l,&r)!=2)
+        return;
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
