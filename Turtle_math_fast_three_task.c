e80b2d11d889b155c0d5115f26fe122b8d5b43b1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-09 10:00:04 +0600

    Update

--- Turtle_math_fast_three_task.c
+++ Turtle_math_fast_three_task.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
+void solve(void) {
+    int n;
+    if(scanf("%d",&n)!=1)
+        return;
+    int sum=0;
+    int has_mod1=0;
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
