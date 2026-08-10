f8354ac5d3332e4c963f63139b2572c3ba740a6d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-10 09:08:38 +0600

    Word_game

---
+++ Word_game.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main() {
+    int t;
+    if (scanf("%d", &t) == 1) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
