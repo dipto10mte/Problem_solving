d3a5db3de2c2ccb6d54cbdc2a2db313d490053a0
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-11 14:09:57 +0600

    Update

--- Line_breaks.c
+++ Line_breaks.c
@@ -6,7 +6,17 @@ void solve(){
     int count=0;
     int current_len=0;
     int stop=0;
-
+    for (int i=0;i<n;i++){
+        char s[501];
+        scanf("%s",s);
+        int len=strlen(s);
+        if(!stop&&current_len+len<=m){
+            current_len+=len;
+            count++;
+        }
+        else
+            stop=1;
+    }
     printf("%d\n",count);
 }
 int main(){
