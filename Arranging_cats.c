68d1d6c48faf216712a13f5adcfb1e0b803a096e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-12 09:51:06 +0600

    Update

--- Arranging_cats.c
+++ Arranging_cats.c
@@ -5,7 +5,10 @@ void solve(){
     char s[100005],t[100005];
     scanf("%s %s",s,t);
     int missing=0,extra=0;
-
+    for (int i=0;i<n;i++){
+        if(s[i]=='0'&&t[i]=='1') missing++;
+        if(s[i]=='1'&&t[i]=='0') extra++;
+    }
     int ans=(missing>extra)?missing:extra;
     printf("%d\n",ans);
 }
