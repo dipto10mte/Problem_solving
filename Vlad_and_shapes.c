c7d8f21949ed70ac9ca2398ee7bab6b27e7d0625
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-13 10:44:13 +0600

    Update

--- Vlad_and_shapes.c
+++ Vlad_and_shapes.c
@@ -4,6 +4,13 @@ int main(){
     scanf("%d",&t);
     while(t--){
         scanf("%d",&n);
+        char g[20];
+        int c[20],k=0;
+        for(int i=0;i<n;i++){
+            scanf("%s",g);
+        int x=0;
+        for(int j=0;j<n;j++)if(g[j]=='1')
+            x++;
 
     }
 return 0;
