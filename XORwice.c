2d31df5ef8736ac54a93c95e0c98bb3a56a52994
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-14 11:38:29 +0600

    Update

--- XORwice.c
+++ XORwice.c
@@ -4,7 +4,8 @@ int main(){
     if(scanf("%d",&t)==1){
         while(t--){
             long long a,b;
-
+            scanf("%lld %lld",&a,&b);
+            printf("%lld\n",a^b);
         }
     }
     return 0;
