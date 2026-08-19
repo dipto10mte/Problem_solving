517af7199618e0d8476bacd213f03d5e2739d9fa
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-19 08:29:22 +0600

    Update

--- Twice.c
+++ Twice.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+void solve(){
+    int n;
+    if(scanf("%d",&n)!=1)
+        return;
+    int count[25]={0};
 
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
