98079095e13b109e4a4e6ccdad2b8be4e4dcc98a
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-23 09:05:49 +0600

    Update

--- Print_a_pedestal_(Codeforces_logo).c
+++ Print_a_pedestal_(Codeforces_logo).c
@@ -3,8 +3,17 @@ void solve(){
     int n;
     scanf("%d",&n);
     int x,y,z;
+    if(n%3==0){
+        x=n/3+1;
+        y=n/3;
+        z=n/3-1;
+    }
+    else if(n%3==1){
+        x=n/3+2;
+        y=n/3;
+        z=n/3-1;
+    }
 
-}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
