2b927ba23d7afdc731a4775ecd8c41641e68dfb2
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-25 12:36:16 +0600

    Update

--- Combination_lock.c
+++ Combination_lock.c
@@ -5,6 +5,10 @@ int main(){
     scanf("%d",&n);
     char s1[1005],s2[1005];
     scanf("%s %s",s1,s2);
-
+    for(int i=0; i<n;i++){
+        int d=abs(s1[i]-s2[i]);
+        ans+=(d<10-d)?d:(10-d);
+    }
+    printf("%d\n",ans);
     return 0;
 }
