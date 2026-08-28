24a3fe01ef9bb244339da8cb6bb593126450f2b7
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-28 10:19:55 +0600

    Update

--- Preparing_for_the_olympiad.c
+++ Preparing_for_the_olympiad.c
@@ -3,7 +3,15 @@ void solve(){
     int n;
     scanf("%d",&n);
     int a[n],b[n];
-
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    int total_diff=a[n-1];
+    for(int i=0;i<n-1;i++){
+        if(a[i]>b[i+1])
+            total_diff+=(a[i]-b[i+1]);
+    }
     printf("%d\n",total_diff);
 }
 int main(){
