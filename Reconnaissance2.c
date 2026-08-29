03aae8fc74f644ee0b42d121488312c4f7e9d7e0
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-29 10:49:31 +0600

    Update

--- Reconnaissance2.c
+++ Reconnaissance2.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
+    int n;
+    if(scanf("%d",&n)!=1)
+        return 1;
+    int a[105];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
 
     printf("%d %d\n",idx1,idx2);
     return 0;
