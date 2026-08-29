61d90328b44d53cddf158b555cea4c0613ef5b7d
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-29 10:49:49 +0600

    Update

--- Reconnaissance2.c
+++ Reconnaissance2.c
@@ -7,7 +7,12 @@ int main(){
     int a[105];
     for(int i=0;i<n;i++)
         scanf("%d",&a[i]);
+    int min_diff=abs(a[0]-a[n-1]);
+    int idx1=1;
+    int idx2=n;
+    for(int i=0;i<n-1;i++){
 
+    }
     printf("%d %d\n",idx1,idx2);
     return 0;
 }
