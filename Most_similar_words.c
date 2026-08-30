f6df7b3ae2c71714a644d2b1b253fb48a8945b3f
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-30 10:14:49 +0600

    Update

--- Most_similar_words.c
+++ Most_similar_words.c
@@ -12,7 +12,15 @@ int main(){
         for(int i=0;i<n;i++)
             scanf("%s",s[i]);
         int min_diff = INT_MAX;
-
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++){
+                int diff=0;
+                for(int k=0;k<m;k++)
+                    diff+=abs(s[i][k]-s[j][k]);
+                if(diff<min_diff)
+                    min_diff=diff;
+            }
+        }
         printf("%d\n",min_diff);
     }
     return 0;
