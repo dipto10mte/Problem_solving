03b7893f2a50679e1bedaaa607cfdd1c9c82c98e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-08-31 07:46:46 +0600

    Update

--- Tournament.c
+++ Tournament.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+void solve(){
+    int n,j,k;
+    if(scanf("%d %d %d",&n,&j,&k)!=3)
+        return;
+    int target_val=0;
+    int max_val=0;
 
+    if(k>1||target_val==max_val)
+        printf("YES\n");
+    else
+        printf("NO\n");
+}
 int main(){
     int t;
     if(scanf("%d",&t)!=1)
