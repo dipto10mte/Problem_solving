f2c26d210f59bf92fef31e24ee694e9c90719bc1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-01 15:26:51 +0600

    Update

--- The_third_side.c
+++ The_third_side.c
@@ -4,7 +4,13 @@ void solve(){
     if(scanf("%d",&n)!=1)
         return;
     long long sum=0;
-
+    for(int i=0;i<n;i++){
+        long long a;
+        scanf("%lld",&a);
+        sum+=a;
+    }
+    long long ans=sum-(n-1);
+    printf("%lld\n",ans);
 }
 int main(){
     int t;
