d74d3b536a4358f128122791251351404341d4ab
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-02 22:59:37 +0600

    Update

--- Sasha_and_the_beautiful_array.c
+++ Sasha_and_the_beautiful_array.c
@@ -5,7 +5,14 @@ void solve(){
         return;
     long long min_val=2e9;
     long long max_val=-1;
-
+    for(int i=0;i<n;i++){
+        long long val;
+        scanf("%lld",&val);
+        if(val<min_val)
+            min_val = val;
+        if(val>max_val)
+            max_val = val;
+    }
     printf("%lld\n",max_val-min_val);
 }
 int main(){
