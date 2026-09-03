1d1281dc66c2fc2778a8c16d7e2b67fada816040
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-03 09:27:24 +0600

    Update

--- Musical_puzzle.c
+++ Musical_puzzle.c
@@ -4,7 +4,18 @@ void solve(){
     int n;
     scanf("%d",&n);
     char s[n+1];
-
+    scanf("%s",s);
+    int seen[26][26]={0};
+    int distinct_pairs=0;
+    for(int i=0;i<n-1;i++){
+        int u =s[i]-'a';
+        int v=s[i+1]-'a';
+        if(!seen[u][v]){
+            seen[u][v]=1;
+            distinct_pairs++;
+        }
+    }
+    printf("%d\n",distinct_pairs);
 }
 int main(){
     int t;
