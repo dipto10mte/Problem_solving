79d9343315774a47a506208ef2bd268e39b74615
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-05 09:49:09 +0600

    Update

--- Rudolph_and_cut_the_rope.c
+++ Rudolph_and_cut_the_rope.c
@@ -6,7 +6,13 @@ int main(){
     while(t--){
         int n,count=0;
         scanf("%d",&n);
-
+        for(int i=0;i<n;i++){
+            int a,b;
+            scanf("%d %d",&a,&b);
+            if(a>b)
+                count++;
+        }
+        printf("%d\n",count);
     }
     return 0;
 }
