e8ab199b17b93346f061ed0635c0cc0085183ff6
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-06 09:01:41 +0600

    Update

--- Symmetric_encoding.c
+++ Symmetric_encoding.c
@@ -4,6 +4,16 @@ int main(){
     int t;
     if(scanf("%d",&t)!=1)
         return 0;
+    while(t--){
+        int n;
+        scanf("%d",&n);
+        char s[200005];
+        scanf("%s",s);
+        int seen[26]={0};
+        for(int i=0;i<n;i++)
+            seen[s[i]-'a']=1;
+        char r[27];
 
+    }
     return 0;
 }
