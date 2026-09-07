0e09552704ee6bf44c623ccaf1ea99b39f02ed6e
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-07 07:02:26 +0600

    Update

--- Riptide.c
+++ Riptide.c
@@ -3,6 +3,37 @@ int main(){
     int t;
     if(scanf("%d",&t)!=1)
         return 0;
-
+    while(t--){
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        int rounds=0;
+        while(1){
+            if(a==b||b==c||a==c)
+                break;
+            int mx=a,mn=a;
+            if(b>mx)
+                mx=b;
+            if(c>mx)
+                mx=c;
+            if(b<mn)
+                mn=b;
+            if(c<mn)
+                mn=c;
+            if(a==mx)
+                a--;
+            else if(b==mx)
+                b--;
+            else
+                c--;
+            if(a==mn)
+                a++;
+            else if
+                (b==mn)b++;
+            else
+                c++;
+            rounds++;
+        }
+        printf("%d\n",rounds);
+    }
     return 0;
 }
