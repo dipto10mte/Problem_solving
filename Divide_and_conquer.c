914204a2b9c45079ada5f2410799fdf69e41d309
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-08 13:25:48 +0600

    Update

--- Divide_and_conquer.c
+++ Divide_and_conquer.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
+    int t;
+    if(scanf("%d",&t)!=1)
+        return 0;
+    while(t--){
+        int x,y;
+        scanf("%d %d",&x,&y);
 
+    }
     return 0;
 }
