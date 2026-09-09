1c87c11d53b447d71cd58f1e68b994aa1d5f25a8
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-09 09:02:10 +0600

    Update

--- Find_and_replace.c
+++ Find_and_replace.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<string.h>
+void solve(){
+    int n;
+    if(scanf("%d",&n)!=1)
+        return;
+    char s[2005];
+    scanf("%s",s);
+    int parity[26];
 
+    if(possible)
+        printf("YES\n");
+    else
+        printf("NO\n");
+}
 int main(){
     int t;
     if(scanf("%d",&t)==1){
