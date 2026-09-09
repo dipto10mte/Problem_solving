7c662d292e27c5272a2c3c4ca3ebb643567228e5
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-09 09:01:46 +0600

    Update

--- Find_and_replace.c
+++ Find_and_replace.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
 
+int main(){
+    int t;
+    if(scanf("%d",&t)==1){
+        while(t--){
+            solve();
+        }
+    }
+    return 0;
+}
