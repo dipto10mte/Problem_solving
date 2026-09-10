098dc7b3c46f937113fee16de3e7cd3947acb5b1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-10 09:33:30 +0600

    Update

--- Polycarp_and_sums_of_subsequences.c
+++ Polycarp_and_sums_of_subsequences.c
@@ -3,7 +3,10 @@ void solve(){
     long long b[7];
     for(int i=0;i<7;i++)
         scanf("%lld",&b[i]);
-
+    long long a1=b[0];
+    long long a2=b[1];
+    long long a3=b[6]-a1-a2;
+    printf("%lld %lld %lld\n",a1,a2,a3);
 }
 int main(){
     int t;
