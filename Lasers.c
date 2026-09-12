1dff7cea44428a9c8c69875874371d58f424a369
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-12 09:17:30 +0600

    Update

--- Lasers.c
+++ Lasers.c
@@ -4,7 +4,14 @@ void solve(){
     long long x,y;
     if(scanf("%d %d %lld %lld",&n,&m,&x,&y)!=4)
         return;
-
+    for(int i=0;i<n;i++){
+        long long a;
+        scanf("%lld",&a);
+    }
+    for(int i=0;i<m;i++){
+        long long b;
+        scanf("%lld",&b);
+    }
     printf("%d\n",n+m);
 }
 int main(){
