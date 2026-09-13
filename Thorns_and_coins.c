b5fb72453974fea1bddd7127dd187cc4754f2a72
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-09-13 21:01:17 +0600

    Update

--- Thorns_and_coins.c
+++ Thorns_and_coins.c
@@ -5,7 +5,13 @@ void solve(){
         return;
     char s[55];
     scanf("%s",s);
-
+    int coins=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='*'&&i+1<n&&s[i+1]=='*')
+            break;
+        if(s[i]=='@')
+            coins++;
+    }
     printf("%d\n",coins);
 }
 int main(){
